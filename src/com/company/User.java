ca3d06935677af5f4955ac88816fbf55bd03b98f
ryankielty <rpkielty@gmail.com>
2017-02-16 14:15:51 -0600

    practice

---
+++ src/com/company/User.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by ryankielty on 12/7/16.
+ */
+public class User {
+    int id;
+    String name;
+    String password;
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+//    public User (String name) {
+//        this.name = name;
+//    }
+}
